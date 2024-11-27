c50fa93176032ec7ee58902a8f817f637c4fc6de
rusisg <rasul.nulrybekov@gmail.com>
2024-11-27 23:16:45 +0100

    fetch items

--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -2,8 +2,11 @@ package fetcher
 
 import (
 	"context"
+	"go.tomakado.io/containers/set"
+	"log"
 	"news_feed_bot/internal/model"
-	source2 "news_feed_bot/internal/source"
+	source "news_feed_bot/internal/source"
+	"strings"
 	"sync"
 	"time"
 )
@@ -39,6 +42,26 @@ func New(articleStorage ArticleStorage, sourceProvider SourceProvider, fetchInte
 	}
 }
 
+func (f *Fetcher) Start(ctx context.Context) error {
+	ticker := time.NewTicker(f.fetchInterval)
+	defer ticker.Stop()
+
+	if err := f.Fetch(ctx); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			if err := f.Fetch(ctx); err != nil {
+				return err
+			}
+		}
+	}
+}
+
 func (f *Fetcher) Fetch(ctx context.Context) error {
 	sources, err := f.sources.Sources(ctx)
 	if err != nil {
@@ -50,11 +73,58 @@ func (f *Fetcher) Fetch(ctx context.Context) error {
 	for _, src := range sources {
 		wg.Add(1)
 
-		rssSource := source2.NewRSSSourceFromModel(src)
+		rssSource := source.NewRSSSourceFromModel(src)
 
 		go func(source Source) {
 			defer wg.Done()
+
+			items, err := source.Fetch(ctx)
+			if err != nil {
+				log.Printf("fetch article %d error: %v", source.ID(), err)
+				return
+			}
+
+			if err := f.processItems(ctx, source, items); err != nil {
+				log.Printf("process article %d error: %v", source.ID(), err)
+				return
+			}
 		}(rssSource)
 	}
+	wg.Wait()
+
 	return nil
+}
+
+func (f *Fetcher) processItems(ctx context.Context, source Source, items []model.Item) error {
+	for _, item := range items {
+		item.Date = item.Date.UTC()
+
+		if f.itemShouldBeSkipped(item) {
+			continue
+		}
+
+		if err := f.articles.Store(ctx, model.Article{
+			SourceID:    source.ID(),
+			Title:       item.Title,
+			Link:        item.Link,
+			Summary:     item.Summary,
+			PublishedAt: item.Date,
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (f *Fetcher) itemShouldBeSkipped(item model.Item) bool {
+	categoriesSet := set.New(item.Categories...)
+
+	for _, keyword := range f.filterKeywords {
+		titleContainsKeyword := strings.Contains(strings.ToLower(item.Title), keyword)
+
+		if categoriesSet.Contains(keyword) || titleContainsKeyword {
+			return true
+		}
+	}
+	return false
 }
