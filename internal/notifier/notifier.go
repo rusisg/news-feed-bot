a51dfc170589796d92d498a930b418d40adb6b3c
rusisg <rasul.nulrybekov@gmail.com>
2024-11-28 14:22:43 +0100

    notifier in process(summarizer, sendArticle)

--- internal/notifier/notifier.go
+++ internal/notifier/notifier.go
@@ -1,1 +1,104 @@
 package notifier
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-shiori/go-readability"
+	"io"
+	"net/http"
+	"news_feed_bot/internal/model"
+	"strings"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type ArticleProvider interface {
+	AllNotPosted(ctx context.Context, since time.Time, limit uint64) ([]model.Article, error)
+	MarkPosted(ctx context.Context, id int64) error
+}
+
+type Summarizer interface {
+	Summarize(ctx context.Context, text string) (string, error)
+}
+
+type Notifier struct {
+	articles         ArticleProvider
+	summarizer       Summarizer
+	bot              *tgbotapi.BotAPI
+	sendInterval     time.Duration
+	lookupTimeWindow time.Duration
+	channelID        int64
+}
+
+func New(
+	articleProvider ArticleProvider,
+	summarizer Summarizer,
+	bot *tgbotapi.BotAPI,
+	sendInterval time.Duration,
+	lookupTimeWindow time.Duration,
+	channelID int64,
+) *Notifier {
+	return &Notifier{
+		articles:         articleProvider,
+		summarizer:       summarizer,
+		bot:              bot,
+		sendInterval:     sendInterval,
+		lookupTimeWindow: lookupTimeWindow,
+		channelID:        channelID,
+	}
+}
+
+func (n *Notifier) SelectAndSendArticle(ctx context.Context) error {
+	topOneArticles, err := n.articles.AllNotPosted(ctx, time.Now().Add(-n.lookupTimeWindow), 1)
+	if err != nil {
+		fmt.Printf("failed to select and send article error: %v", err)
+		return err
+	}
+
+	if len(topOneArticles) == 0 {
+		return nil
+	}
+
+	article := topOneArticles[0]
+
+	summary, err := n.extractSummary(ctx, article)
+	if err != nil {
+		fmt.Printf("failed to extract summary error: %v", err)
+		return err
+	}
+
+	if err := n.sendArticle(article, summary); err != nil {
+		fmt.Printf("failed to send article: %v", err)
+		return err
+	}
+
+	return n.articles.MarkPosted(ctx, article.ID)
+}
+
+func (n *Notifier) extractSummary(ctx context.Context, article model.Article) (string, error) {
+	var r io.Reader
+
+	if article.Summary != "" {
+		r = strings.NewReader(article.Summary)
+	} else {
+		resp, err := http.Get(article.Link)
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+
+		r = resp.Body
+	}
+
+	doc, err := readability.FromReader(r, nil)
+	if err != nil {
+		return "", err
+	}
+
+	summary, err := n.summarizer.Summarize(ctx, doc.TextContent)
+
+	return "\n\n" + summary, nil
+}
+
+func (n *Notifier) sendArticle(article model.Article, summary string) error {}
