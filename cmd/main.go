a6ad21064e2c7212c2bc70c6ea84f1b1105f370e
rusisg <rasul.nulrybekov@gmail.com>
2024-11-30 15:27:30 +0100

    cmd main run

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"errors"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
+	"golang.org/x/net/context"
+	"log"
+	"news_feed_bot/internal/config"
+	fetcher "news_feed_bot/internal/fetcher"
+	"news_feed_bot/internal/notifier"
+	"news_feed_bot/internal/storage"
+	"news_feed_bot/internal/summary"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	botAPI, err := tgbotapi.NewBotAPI(config.Get().TelegramBotToken)
+	if err != nil {
+		log.Printf("Failed to create to telegram bot: %v", err)
+		return
+	}
+
+	db, err := sqlx.Connect("postgres", config.Get().DatabaseDSN)
+	if err != nil {
+		log.Printf("Failed to connect to database: %v", err)
+		return
+	}
+	defer db.Close()
+
+	var (
+		articleStorage = storage.NewArticleStorage(db)
+		sourceStorage  = storage.NewSourceStorage(db)
+		fetcher        = fetcher.New(
+			articleStorage,
+			sourceStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+		notifier = notifier.New(
+			articleStorage,
+			summary.NewOpenAISummarizer(config.Get().OpenAiKey, config.Get().OpenAiPrompt),
+			botAPI,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] Failed to start fetcher: %v", err)
+				return
+			}
+		}
+		log.Println("fetcher stopped")
+	}(ctx)
+
+	//go func(ctx context.Context) {
+	if err := notifier.Start(ctx); err != nil {
+		if !errors.Is(err, context.Canceled) {
+			log.Printf("[ERROR] Failed to start notifier: %v", err)
+			return
+		}
+	}
+	log.Println("notifier stopped")
+	//}(ctx)
+}
